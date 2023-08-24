d857903a88bb4d23f7e4db9eb8909004612e44fd
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-08-24 12:19:55 +0200

    deploy test

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route :: prefix('v1') -> group(function() {
+
+    Route :: get('/test', function() {
+        return response() -> json([
+            'message' => 'Hello World!'
+        ], 200);
+    });
+});
