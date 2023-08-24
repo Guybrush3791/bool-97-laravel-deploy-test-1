6d6149f4b75898379f6b0c6a9f2a25978d5b301d
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-08-24 13:30:10 +0200

    $*

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TestController;
 
 Route :: get('/', [TestController :: class, 'testIndex']);
+Route :: get('/prova', function() { return 'Prova'; });
