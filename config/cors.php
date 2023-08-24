124eeea7cef1732d16055be93e70109c17fa0b4a
Giovanni Cappelletti <giovanni.cappelletti.1973@gmail.com>
2023-08-24 14:03:36 +0200

    $*

--- config/cors.php
+++ config/cors.php
@@ -17,9 +17,9 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'OPTIONS'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://64.226.94.44/'],
 
     'allowed_origins_patterns' => [],
 
